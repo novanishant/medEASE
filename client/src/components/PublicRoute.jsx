3e88dc436ed2b642f89c03284e9f2bb5eec09b39
novanishant <nishantverma3633@gmail.com>
2024-03-30 12:48:13 +0530

    final code for deployment
    deployment

--- client/src/components/PublicRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function PublicRoute({ children }) {
-  if (localStorage.getItem("token")) {
-    return < Navigate to="/" />;
-  } else {
-    return children;
-  }
-}
